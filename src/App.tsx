8780af85d8a1551616c3d50d1959161c141b2798
Valentin <razumny.valentin@gmail.com>
2025-02-25 10:55:08 +0300

    new fix input

--- src/App.tsx
+++ src/App.tsx
@@ -32,7 +32,10 @@ function App() {
   // POST
 
   const addTask = async () => {
-    if (!inputName.trim()) return;
+    if (!inputName.trim()) {
+      alert("название задачи не может быть пустым");
+      return;
+    }
     if (inputName.length < 2 || inputName.length > 64) {
       alert("название задачи должно быть от 2 до 64 символов");
       return;
