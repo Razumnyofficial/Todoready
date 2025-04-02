02fd5e0e678bcdf1525e6f09e4306bac3f65c450
Valentin <razumny.valentin@gmail.com>
2025-04-02 23:14:23 +0300

    мсг

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ const router = createBrowserRouter([
     element: <RootLayout />,
     errorElement: <ErrorPage />,
     children: [
-      { path: "/", element: <PageTodo /> },
+      { index: true, element: <PageTodo /> },
       { path: "/auth", element: <PageAuth /> },
     ],
   },
