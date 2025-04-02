2126172f89734822b02fc3e00529d1c8fd005aaa
Valentin <razumny.valentin@gmail.com>
2025-04-02 22:38:55 +0300

    fix 404

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -2,11 +2,12 @@ import MainNavigation from "../components/MainNavigation/MainNavigation";
 
 function ErrorPage() {
   return (
-    <main>
+    <>
       <MainNavigation />
-      <h1>404 Not Found</h1>
-      <p>Could not find this page</p>
-    </main>
+      <main>
+        <h1>404 Not Found</h1>
+      </main>
+    </>
   );
 }
 
