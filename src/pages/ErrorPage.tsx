c0fddfee269da503bbdfdb4b3cf2644b2f128405
Valentin <razumny.valentin@gmail.com>
2025-04-10 03:41:34 +0300

    fix 404 error styles

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,11 +1,17 @@
-import MainNavigation from "../components/MainNavigation/MainNavigation";
+import { Link } from "react-router-dom";
+// import MainNavigation from "../components/MainNavigation/MainNavigation";
 
 function ErrorPage() {
   return (
     <>
-      <MainNavigation />
-      <main>
+      {/* <MainNavigation /> */}
+      <main style={{ display: "block", textAlign: "center", padding: "20%" ,}}>
         <h1>404 Not Found</h1>
+        <button style={{ padding: "20px" }}>
+          <Link to="/login" style={{ marginLeft: 0 }}>
+            Авторизируйся
+          </Link>
+        </button>
       </main>
     </>
   );
