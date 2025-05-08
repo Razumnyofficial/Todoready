75f959bd58bc94c25abb68f606263fab09ae92f8
Valentin <razumny.valentin@gmail.com>
2025-05-08 04:20:02 +0300

    fix button users

--- src/components/SideMenu.tsx
+++ src/components/SideMenu.tsx
@@ -1,10 +1,27 @@
 import { Layout, Menu } from "antd";
 import { Link, useLocation } from "react-router-dom";
+import { User } from "@/types/usersTypes";
+import { useEffect, useState } from "react";
+import { getUser } from "@/api/auth";
+import { Roles } from "@/types/usersTypes";
 
 const { Sider } = Layout;
 
 const SideMenu = () => {
+  const [currentUser, setCurrentUser] = useState<User | null>(null);
   const location = useLocation();
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const data = await getUser();
+        setCurrentUser(data);
+      } catch (error) {
+        console.error("Error fetching user:", error);
+      }
+    };
+    fetchUser();
+  }, []);
 
   const menuItems = [
     {
@@ -19,10 +36,10 @@ const SideMenu = () => {
       key: "/profile",
       label: <Link to="/profile">Profile</Link>,
     },
-    {
+    ...(currentUser?.roles?.includes(Roles.ADMIN) ? [{
       key: "/users",
       label: <Link to="/users">Users</Link>
-    }
+    }] : [])
   ];
 
   return (
